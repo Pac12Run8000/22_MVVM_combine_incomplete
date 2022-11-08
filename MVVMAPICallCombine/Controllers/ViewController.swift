d169ecb0b1cb8dbabb9416094582235f45bcba8a
Norbert Grover <norbert298@gmail.com>
2022-11-08 14:24:22 -0800

    Beginning setup

--- MVVMAPICallCombine/Controllers/ViewController.swift
+++ MVVMAPICallCombine/Controllers/ViewController.swift
@@ -9,14 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var searchTextField: UITextField!
+    let tempArray = ["Floyd Schofield", "Dimitri Grover", "Major Hacket", "Lorenzo Simpson", "Tank Davis"]
     
+    @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var tableView: UITableView!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     @IBAction func submitBtnPressed(_ sender: UIButton) {
@@ -24,5 +26,18 @@ class ViewController: UIViewController {
         
     }
     
+}
+
+extension ViewController:UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tempArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = tempArray[indexPath.row]
+        return cell!
+    }
 }
 
