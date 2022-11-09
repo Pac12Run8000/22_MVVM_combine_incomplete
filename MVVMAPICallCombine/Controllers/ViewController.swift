67d16040a8c42f97d292a7c47dd51134c3d4ec4d
Norbert Grover <norbert298@gmail.com>
2022-11-09 10:16:51 -0800

    Incomplete

--- MVVMAPICallCombine/Controllers/ViewController.swift
+++ MVVMAPICallCombine/Controllers/ViewController.swift
@@ -6,27 +6,27 @@
 //
 
 import UIKit
-import Combine
+
 
 class ViewController: UIViewController {
     
-    var cancellables:Set<AnyCancellable> = []
+   let tempArray = ["California", "Texas", "Michigan", "Maryland"]
     
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var tableView: UITableView!
     
-    var viewModel = ViewModel()
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        setupBinding()
+        
         
 
     }
 
     @IBAction func submitBtnPressed(_ sender: UIButton) {
-        viewModel.populateTableViewWithList(input: searchTextField.text)
+        
     }
     
 }
@@ -34,24 +34,15 @@ class ViewController: UIViewController {
 extension ViewController:UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.list.count
+        return tempArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
-        cell?.textLabel?.text = viewModel.list[indexPath.row]
+        cell?.textLabel?.text = tempArray[indexPath.row]
         return cell!
     }
 }
 
-extension ViewController {
-    
-    public func setupBinding() {
-        viewModel.$list.sink {  _ in
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
-            }
-        }.store(in: &cancellables)
-    }
-}
+
 
--- MVVMAPICallCombine/Controllers/ViewModel.swift
+++ MVVMAPICallCombine/Controllers/ViewModel.swift
@@ -11,35 +11,7 @@ import Combine
 
 final class ViewModel {
     
-    @Published var data:Data?
-    @Published var acronymObject:AcronymObject?
-    @Published var list:[String] = []
     
-    var cancellables:Set<AnyCancellable> = []
-    
-    public func populateTableViewWithList(input:String?) {
-        var localList = [String]()
-        guard let input = input, let url = URLComponentConstants.createURLWithComponents(queryParameters: [URLQueryName.sf.rawValue:input])?.url else {
-            print(ResponseError.invalidUrl.description)
-            return
-        }
-        
-        let promise = NetworkingService.shared.fetchAPIResponse(url: url)
-        let acronym = promise.decode(type: AcronymObject.self, decoder: JSONDecoder())
-        acronym.sink { (completion) in
-            if case let .failure(error) = completion {
-                print("ViewModel Error: \(error.localizedDescription)")
-            }
-        } receiveValue: { [weak self] acronym in
-            guard let strongself = self else {return}
-            strongself.acronymObject = acronym
-            for item in acronym[0].lfs {
-                localList.append(item.lf)
-            }
-            strongself.list = localList
-        }.store(in: &cancellables)
-
-    }
     
 }
 
